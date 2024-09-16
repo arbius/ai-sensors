f96fae6a8febe64cde030c13020da48d60df667c
Rick Binks <arbius1@gmail.com>
2024-09-16 16:50:34 -0700

    tinker with json

---
+++ shell3.py
@@ -0,0 +1,27 @@
+# use this shell for setting the external RTC
+
+print ('entering shell3')
+
+# import connect
+# connect.do_connect()
+# import setDS3231
+# import getDS3231
+
+# import dthmain
+import os
+import cat
+
+# dthmain.main()
+os.listdir()
+# cat.cat('unsent_data.json')
+import ai3
+import getjson
+
+
+
+
+
+print ('done with shell3')
+
+
+
