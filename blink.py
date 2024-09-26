656c1b8c56a3b6045232ec1176bdcf176ee6769d
Rick Binks <arbius1@gmail.com>
2024-09-26 11:42:59 -0700

    make the function callable with variable number of blinks

--- blink.py
+++ blink.py
@@ -14,7 +14,6 @@ NO_NETWORK = 3
 def blink_error(short_blinks):
        
         # The more verbose way
-    while True:
         led_pin.value(1)
         sleep_ms(1000)
         for i in range (short_blinks):
@@ -25,6 +24,8 @@ def blink_error(short_blinks):
         
         led_pin.value(0)
         sleep_ms(2000)
+
+        return
 
 
 # blink_error(5)
