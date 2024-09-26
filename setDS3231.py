de17894aca4cf6cc6bf267a8711e7ba052030425
Rick Binks <arbius1@gmail.com>
2024-09-26 11:16:13 -0700

    set to utc minus 7 for MST, supress pylance warning for time.mktime(utc_time)

--- setDS3231.py
+++ setDS3231.py
@@ -1,4 +1,13 @@
-setDS3231
+# setDS3231
+
+# Problems connecting with network.
+# From REPL, used
+# import connect
+# connect.mysyntax()
+# connect.do_connect()
+# import setDS3231
+# and it worked
+
 import network
 import ntptime
 import time
@@ -6,8 +15,13 @@ from machine import I2C, Pin
 from ds3231 import DS3231
 
 # 1. Connect to Wi-Fi
-SSID = 'your_SSID'
-PASSWORD = 'your_password'
+SSID = 'Galaxy A25 5G 6737'
+PASSWORD = '1111111'
+
+# # Set up network credentials
+# ssid = "Galaxy A25 5G 6737"
+# password = "11111111"
+
 
 def connect_wifi(ssid, password):
     wlan = network.WLAN(network.STA_IF)
@@ -25,7 +39,7 @@ def get_local_time(offset_hours):
     utc_time = time.localtime()
     
     # Adjust UTC to local time by adding the time zone offset
-    local_time = time.mktime(utc_time) + (offset_hours * 3600)
+    local_time = time.mktime(utc_time) + (offset_hours * 3600) # type: ignore
     return time.localtime(local_time)
 
 # 3. Set local time to the DS3231
@@ -40,10 +54,10 @@ def main():
     connect_wifi(SSID, PASSWORD)
 
     # Define I2C bus for DS3231 (adjust SDA and SCL pins as needed)
-    i2c = I2C(1, scl=Pin(22), sda=Pin(21))
+    i2c = I2C(1, scl=Pin(16), sda=Pin(26))
 
     # Get the local time, e.g., offset of +2 hours for UTC+2 timezone
-    local_time = get_local_time(offset_hours=2)
+    local_time = get_local_time(offset_hours=-7)
     
     # Set the local time to DS3231
     set_ds3231_time(i2c, local_time)
